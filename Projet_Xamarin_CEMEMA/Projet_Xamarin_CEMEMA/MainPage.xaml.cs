9ca62c0c8e2e57c05f3bfec5e7af594ef3c3885d
Tokitokivodka <54808191+Tokitokivodka@users.noreply.github.com>
2021-04-05 20:24:11 +0200

    Init commit start project

---
+++ Projet_Xamarin_CEMEMA/Projet_Xamarin_CEMEMA/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Projet_Xamarin_CEMEMA
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
