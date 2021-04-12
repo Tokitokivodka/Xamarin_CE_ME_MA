28a3423f239c30df981af81020fea19270b7e984
Tokitokivodka <54808191+Tokitokivodka@users.noreply.github.com>
2021-04-12 20:40:11 +0200

    Ajout du HttpService et restructuration de l'arborescence du projet

---
+++ Projet_Xamarin_CEMEMA/Projet_Xamarin_CEMEMA/Services/HttpService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Projet_Xamarin_CEMEMA.Services
+{
+    public static class HttpService
+    {
+        static HttpClient client;
+
+        public static HttpClient GetInstance()
+        {
+            if (client == null)
+            {
+                client = new HttpClient();
+            }
+            return client;
+        }
+    }
+}
