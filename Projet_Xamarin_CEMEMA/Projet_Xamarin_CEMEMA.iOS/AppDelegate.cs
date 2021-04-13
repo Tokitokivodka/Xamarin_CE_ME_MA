c37e233180d8594108da76687a87d0fc44012e98
elo <elodie@mbp-de-mendel.home>
2021-04-13 13:11:19 +0200

    Add package nuget and init map

--- Projet_Xamarin_CEMEMA/Projet_Xamarin_CEMEMA.iOS/AppDelegate.cs
+++ Projet_Xamarin_CEMEMA/Projet_Xamarin_CEMEMA.iOS/AppDelegate.cs
@@ -23,6 +23,7 @@ namespace Projet_Xamarin_CEMEMA.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+            global::Xamarin.FormsMaps.Init();
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
