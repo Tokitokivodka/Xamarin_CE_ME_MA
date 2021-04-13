9f67546f525785d614ec9f61aca16865f7f4014e
Marina-Csn <marina.cesano@gmail.com>
2021-04-13 12:42:54 +0200

    Ajout des fonctionnalité profil et navbar

---
+++ Projet_Xamarin_CEMEMA/Projet_Xamarin_CEMEMA/Views/NavigationBarPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Projet_Xamarin_CEMEMA
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NavigationBarPage : TabbedPage
+    {
+        public NavigationBarPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Projet_Xamarin_CEMEMA/Projet_Xamarin_CEMEMA/Views/PageProfile.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Projet_Xamarin_CEMEMA
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageProfile : ContentPage
+    {
+        public ObservableCollection<string> Items { get; set; }
+
+        public PageProfile()
+        {
+            InitializeComponent();
+
+            Items = new ObservableCollection<string>
+            {
+                "More informations",
+                "List Event Created",
+                "List Event Participated"
+            };
+
+            MyListView.ItemsSource = Items;
+        }
+
+        async void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item == null)
+                return;
+
+            if (e.Item.ToString() == "List Event Created")
+            {
+                var newPage = new PageListEventsCreated();
+                await Navigation.PushAsync(newPage);
+            }
+            else if (e.Item.ToString() == "List Event Participated")
+            {
+                var newPage = new PageListEventsParticipated();
+                await Navigation.PushAsync(newPage);
+            }
+
+            //Deselect Item
+            ((ListView)sender).SelectedItem = null;
+        }
+    }
+}
